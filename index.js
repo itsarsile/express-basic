01011c91166947e3be069084d6c6122b32f1ca9e
Izaaz Waskito <116268166+izaazwaskito@users.noreply.github.com>
2023-07-11 14:39:14 +0700

    update

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require("express");
 const createError = require("http-errors");
 const app = express();
 const cors = require("cors");
-const morgan = require("morgan");
+
 const mainRouter = require("./src/routes/index");
 const PORT = process.env.PORT || 5000;
 const DB_HOST = process.env.DB_HOST;
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
+
 app.use("/api/v1", mainRouter);
 app.use("/img", express.static("src/upload"));
 app.all("*", (req, res, next) => {
